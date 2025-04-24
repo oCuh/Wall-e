f1dabead976f6128441d6c6a5d249cb6d03c9436
Sérgio Farias <sergiofariasvd@gmail.com>
2025-04-24 10:46:36 -0300

    MainMenu && Assets
    Main Menu Completo
    Adicionado os Sprites de terreno de grama e tecnológico
    |->TODO: Adicionar sprite de terrno meio a meio

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,1 +1,15 @@
 extends Control
+
+@onready var mainTheme = $mainTheme
+@onready var timer = $Timer
+@onready var startOst = $startOST
+
+func _on_menu_button_pressed():
+	mainTheme.stop()
+	startOst.play()
+
+func _on_start_ost_finished():
+	timer.start()
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
